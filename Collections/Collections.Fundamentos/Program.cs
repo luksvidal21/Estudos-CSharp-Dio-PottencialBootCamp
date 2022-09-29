9c4d3bbe4f2955bdb67a2ddfb179dc817d412618
Lucas Vidal <70353070+luksvidal21@users.noreply.github.com>
2022-09-29 15:23:06 -0300

    Adição de Tópicos
    Adição de mais tópicos abordados no curso

---
+++ Collections/Collections.Fundamentos/Program.cs
@@ -0,0 +1,66 @@
+﻿Queue<int> fila = new Queue<int>();
+
+
+//Exemplos de Filas(Queues) no C#
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+fila.Enqueue(10);
+fila.Enqueue(12);
+
+Console.WriteLine("Itens da Fila:");
+
+foreach(int item in fila)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"O ITEM REMOVIDO DA FILA É : {fila.Dequeue()}");
+
+Console.WriteLine("-----------------\n");
+
+
+
+Stack<int> pilha = new Stack<int>();
+
+pilha.Push(2);
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+pilha.Push(12);
+
+Console.WriteLine("Itens da Pilha:");
+
+foreach(int item in pilha)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"O ITEM REMOVIDO DA PILHA É : {pilha.Pop()}");
+
+Console.WriteLine($"\n-----------------------\n");
+
+
+
+Dictionary<int,string> estados = new Dictionary<int, string>();
+
+estados.Add(1,"Acre");
+estados.Add(2,"Amazonas");
+
+try
+{
+    estados.Add(1,"Acre");
+}
+catch
+{
+    Console.WriteLine("Você tentou adicionar um estado que já existe");
+}
+finally
+{   Console.WriteLine($"O dados do Dicionário são:");
+    foreach(var item in estados)
+    {
+        Console.WriteLine($"Chave : {item.Key} ; Valor : {item.Value} ");
+    }
+}
---
+++ Tuple/Tuple.Fundamentos/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tuple.Fundamentos.Models;
+
+//Primeira forma de declarar uma tupla:
+//(int,string,string,decimal) tupla = (1,"Lucas","Vidal",1.80M);
+//Variação da primeira forma de declarar uma tupla:
+
+//MELHOR FORMA DE DECLARAR UMA TUPLA
+//(int Id, string Nome, string SobreNome , decimal Altura) tupla1variacao = (1,"Lucas","Vidal",1.80M);
+//A maior vantagem dessa variação da primeira forma de declarar é que consigo nomear cada um dos atributos em que declaro um tipo
+
+//Segunda forma de declarar uma tupla:
+//ValueTuple<int,string,string,decimal> tupla2 = (1,"Lucas","Vidal",1.80M);
+
+
+//Terceira forma de declarar uma tupla:
+
+//var tupla3 = Tuple.Create(1,"Lucas","Vidal",1.80M);
+
+
+LeituraArquivo arquivo = new LeituraArquivo();
+
+var (sucesso, linhas, _) = arquivo.LerArquivo("Arquivos/Arquivo_Texto.txt");
+//Caso precise descartar uma das propriedades é possível utilizar o "_"
+
+if(sucesso){
+    //Console.WriteLine($"A quantidade de linha é : {quantidadeDeLinhas}");
+    foreach(string linha in linhas){
+        Console.WriteLine(linha);
+    }
+}
+else 
+{
+    Console.WriteLine("Não foi possível executar o arquivo");
+}
+
