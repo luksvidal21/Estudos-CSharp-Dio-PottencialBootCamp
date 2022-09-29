054033f7946311679ac9f78a83b129c35d04b2dd
Lucas Vidal <70353070+luksvidal21@users.noreply.github.com>
2022-09-29 10:59:32 -0300

    Commit Inicial
    Adicionando primeiras pastas dos projetos feitos no bootcamp

---
+++ Exceptions/Exceptions.Fundamentos/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    
+    String[] arquivo = File.ReadAllLines("Arquivos/Arquivo_Texto.txt");
+
+    foreach(string linhas in arquivo){
+        Console.WriteLine(linhas);
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica: {ex.Message}");
+}
